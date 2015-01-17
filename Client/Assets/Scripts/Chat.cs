82925863a37806ec071eb31f8f6dfd2791620cfe
AlexanderDykov <alexander.dykov@gmail.com>
2015-01-17 22:43:04 +0200

    -Creating Client(TCP)

---
+++ Client/Assets/Scripts/Chat.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour {
+
+    [SerializeField]
+    Text name;
+    [SerializeField]
+    Text message;
+    [SerializeField]
+    Text forPrint;
+    [SerializeField]
+    Scrollbar scroll;
+    public static Text sendMessage;
+    const int PORT = 11000;
+    Client client;
+
+
+    void Start()
+    {
+        Application.runInBackground = true;
+        client = new Client(PORT, "192.168.1.101");
+        client.Work();
+        sendMessage = forPrint;
+    }
+
+    public void PrintMessage()
+    {
+        client.SendMessage(name.text + " : " + message.text);
+        scroll.value = 0;
+    }
+}
---
+++ Client/Assets/Scripts/Client.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System.Collections.Generic;
+
+public class Client : MonoBehaviour {
+
+    static TcpClient client;
+
+    public Client(int Port, string connectIp)
+    {
+        client = new TcpClient();
+        client.Connect(IPAddress.Parse(connectIp), Port);
+    }
+
+    public void Work()
+    {
+        Thread clientListener = new Thread(Reader);
+        clientListener.Start();
+    }
+
+    public void SendMessage(string message)
+    {
+        message.Trim();
+        byte[] Buffer = Encoding.ASCII.GetBytes((message).ToCharArray());
+        client.GetStream().Write(Buffer, 0, Buffer.Length);
+        Chat.sendMessage.text += message + "\n";
+    }
+
+    static void Reader()
+    {
+        while (true)
+        {
+            NetworkStream NS = client.GetStream();
+            List<byte> Buffer = new List<byte>();
+            while (NS.DataAvailable)
+            {
+                int ReadByte = NS.ReadByte();
+                if (ReadByte > -1)
+                {
+                    Buffer.Add((byte)ReadByte);
+                }
+            }
+            if (Buffer.Count > 0)
+                Chat.sendMessage.text +=  Encoding.ASCII.GetString(Buffer.ToArray()) +"\n";
+        }
+    }
+
+    ~Client()
+    {
+        if (client != null)
+        {
+            client.Close();
+        }
+    }
+}
